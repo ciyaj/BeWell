c12734a178c3a4b007e773bb4d17740172f7acae
Ciya Joseph <ciyajoseph05@gmail.com>
2023-10-28 13:33:07 -0400

    Made XCode project for repo

---
+++ BeWell/BeWell/BeWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeWellApp.swift
+//  BeWell
+//
+//  Created by Ciya Joseph on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct BeWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
